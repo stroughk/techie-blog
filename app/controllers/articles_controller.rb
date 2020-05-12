11dfc1def5fd00ffd585b1663f7875eb36400665
stroughk <katerynastrough@hotmail.com>
2020-05-12 17:45:23 -0500

    add index page and form for new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,5 +3,16 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index
+    @articles = Article.all 
+  end
+
+  def new
+
+  end
+
+  def create
+    render plain: params[:article]
+  end 
 
 end
