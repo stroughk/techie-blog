4e267efc333c8672d03ab24380e9ed21e88737f9
stroughk <katerynastrough@hotmail.com>
2020-05-19 17:15:23 -0500

    add create action and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,11 +8,17 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
   end
 
   def create
-    render plain: params[:article]
+    #render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+    redirect_to article_path(@article)
+    else
+    render 'new' 
+    end 
   end 
-
-end
+end 
