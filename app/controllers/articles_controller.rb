5c5ab00c30c682156c36d71f11b369a964829287
stroughk <katerynastrough@hotmail.com>
2020-06-09 17:38:15 -0500

    add helper methods and partial to keep it DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
-  def show 
-    @article = Article.find(params[:id])
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  
+  def show   
   end
 
   def index
@@ -11,13 +12,12 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  def edit
-    @article = Article.find(params[:id])
+  def edit   
   end
 
   def create
     #render plain: params[:article]
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully."
     redirect_to article_path(@article)
@@ -27,8 +27,7 @@ class ArticlesController < ApplicationController
   end 
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice]="Article was successfully updated"
     else 
       render 'edit'
@@ -36,8 +35,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+
+  def set_article   
+     @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end 
